c6a06d2216504693e9f969d53fbfae434ebef125
asutermo <asutermo@mymail.mines.edu>
2012-10-25 16:38:35 -0600

    removed high scores table

--- high_scores_table.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE `high_scores` (
-  `id` int(11) NOT NULL,
-  `player_initials` varchar(3) DEFAULT NULL,
-  `player_picture` longblob,
-  `player_score` double DEFAULT NULL,
-  `score_date` datetime DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1$$
-
