59832242cd768861e4e59b05c1fdebaa1b1887e3
asutermo <asutermo@mymail.mines.edu>
2012-10-27 12:11:38 -0600

    added gitignore file. started working on high scores object

---
+++ src/java/highscores/PlayerHighScoreInformation.java
@@ -0,0 +1,11 @@
+package java.highscores;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Andrew
+ * Date: 10/27/12
+ * Time: 12:06 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class PlayerHighScoreInformation {
+}
