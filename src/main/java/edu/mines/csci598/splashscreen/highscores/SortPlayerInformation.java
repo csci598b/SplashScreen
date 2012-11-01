7c8bab00079c48dc10813083ab4e44620523b6cc
asutermo <asutermo@mymail.mines.edu>
2012-10-31 18:22:51 -0600

    inlined some variables in sort

--- src/main/java/edu/mines/csci598/splashscreen/highscores/SortPlayerInformation.java
+++ src/main/java/edu/mines/csci598/splashscreen/highscores/SortPlayerInformation.java
@@ -7,28 +7,25 @@ import java.util.Comparator;
 public class SortPlayerInformation {
 
     public ArrayList<PlayerHighScoreInformation> sortByScore(ArrayList<PlayerHighScoreInformation> playerHighScoreInformationList) {
-        ArrayList<PlayerHighScoreInformation> playerScores = playerHighScoreInformationList;
-        Collections.sort(playerScores, new Comparator<PlayerHighScoreInformation>() {
+        Collections.sort(playerHighScoreInformationList, new Comparator<PlayerHighScoreInformation>() {
             public int compare(PlayerHighScoreInformation object1, PlayerHighScoreInformation object2) {
                 return Double.compare(object1.getPlayerScore(), object2.getPlayerScore());
             }
         });
-        return playerScores;
+        return playerHighScoreInformationList;
     }
 
     public ArrayList<PlayerHighScoreInformation> sortByInitials(ArrayList<PlayerHighScoreInformation> playerHighScoreInformationList) {
-        ArrayList<PlayerHighScoreInformation> playerScores = playerHighScoreInformationList;
-        Collections.sort(playerScores, PlayerHighScoreInformation.playerInitialsComporator);
-        return playerScores;
+        Collections.sort(playerHighScoreInformationList, PlayerHighScoreInformation.playerInitialsComporator);
+        return playerHighScoreInformationList;
     }
 
     public ArrayList<PlayerHighScoreInformation> sortByTime(ArrayList<PlayerHighScoreInformation> playerHighScoreInformationList) {
-        ArrayList<PlayerHighScoreInformation> playerScores = playerHighScoreInformationList;
-        Collections.sort(playerScores, new Comparator<PlayerHighScoreInformation>() {
+        Collections.sort(playerHighScoreInformationList, new Comparator<PlayerHighScoreInformation>() {
             public int compare(PlayerHighScoreInformation object1, PlayerHighScoreInformation object2) {
                 return Long.compare(object1.getPlayerTime(), object2.getPlayerTime());
             }
         });
-        return playerScores;
+        return playerHighScoreInformationList;
     }
 }
