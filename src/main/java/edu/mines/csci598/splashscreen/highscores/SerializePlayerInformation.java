d57cee431705739cad7c96d4f1e4d4e9a4ac4b21
asutermo <asutermo@mymail.mines.edu>
2012-11-05 22:34:34 -0700

    modified serializing/deserializing to handle storing only 10 and to handle cases where file has not been made

--- src/main/java/edu/mines/csci598/splashscreen/highscores/SerializePlayerInformation.java
+++ src/main/java/edu/mines/csci598/splashscreen/highscores/SerializePlayerInformation.java
@@ -2,31 +2,48 @@ package edu.mines.csci598.splashscreen.highscores;
 
 import java.io.*;
 import java.util.ArrayList;
+import java.util.List;
 
 //Andrew Suter-Morris
 public class SerializePlayerInformation {
 
+    private static final int NUM_PLAYER_SCORES_TO_STORE = 10;
+
     public static ArrayList<PlayerHighScoreInformation> retrievePlayerHighScoreInformation(String fileName) {
-        Object deserializedHighScores = null;
-        try {
-            ObjectInputStream oin = new ObjectInputStream(new FileInputStream(fileName));
-            deserializedHighScores = oin.readObject();
-            oin.close();
+        Object deserializedHighScores = new ArrayList<PlayerHighScoreInformation>();
+        File file = new File(fileName);
+        if (file.isFile() && file.canRead()) {
+            try {
+                ObjectInputStream oin = new ObjectInputStream(new FileInputStream(file));
+                deserializedHighScores = oin.readObject();
+                oin.close();
+            }
+            catch (Exception ioe) {
+                ioe.printStackTrace();
+            }
         }
-        catch (Exception ioe) {
-            ioe.printStackTrace();
-        }
+
         return (ArrayList<PlayerHighScoreInformation>) deserializedHighScores;
     }
 
     public static void storePlayerHighScoreInformation(ArrayList<PlayerHighScoreInformation> highScores, String fileName) {
+        List<PlayerHighScoreInformation> abbreviatedList;
+
+        try {
+            abbreviatedList = highScores.subList(0, NUM_PLAYER_SCORES_TO_STORE - 1);
+        }
+        catch (IndexOutOfBoundsException ioobe) {
+            abbreviatedList = highScores;
+        }
+
         try {
             ObjectOutputStream oout = new ObjectOutputStream(new FileOutputStream(fileName));
-            oout.writeObject(highScores);
+            oout.writeObject(abbreviatedList);
             oout.close();
         }
         catch (IOException ioe) {
             ioe.printStackTrace();
         }
+
     }
 }
