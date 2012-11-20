6e5648092eb030c3664a2d0a1e17a61d236c6323
asutermo <asutermo@mymail.mines.edu>
2012-11-20 16:18:40 -0700

    Added header/footer classes

---
+++ src/main/java/edu/mines/csci598/splashscreen/graphics/InstructionsHeader.java
@@ -0,0 +1,11 @@
+package edu.mines.csci598.splashscreen.graphics;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Andrew
+ * Date: 11/20/12
+ * Time: 4:17 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class InstructionsHeader {
+}
